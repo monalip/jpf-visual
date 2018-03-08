eca86ec70b194af26f9003ec8ac546fdf10a29f6
monalip <monalipande9@gmail.com>
2018-03-08 13:21:24 +0100

    Cahage the import gov.nasa.jpf.util.Left to another import kth.se.Adapters.Left

---
+++ src/main/java/kth/se/Adapters/StepAdapter.java
@@ -0,0 +1,14 @@
+package kth.se.Adapters;
+
+import gov.nasa.jpf.vm.Instruction;
+import kth.se.Interfaces.StepInterface;
+
+public class StepAdapter implements StepInterface{
+
+	@Override
+	public Instruction getInstruction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
