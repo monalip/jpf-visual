7e2d2504293ba8f00e7ec0c63290c1cc88d9bce6
monalip <monalipande9@gmail.com>
2018-05-18 10:50:30 +0200

    ErrorTracePanelGui interface is not need we are calling  function directly without interface

--- src/main/java/se/kth/tracedata/ErrorTracePanelGui.java
+++
@@ -1,8 +0,0 @@
-package se.kth.tracedata;
-
-public interface ErrorTracePanelGui {
-	public void initialization();
-	public void displayErrMsg(boolean err);
-	public void drowErrTrace(Path path, boolean found);
-
-}
